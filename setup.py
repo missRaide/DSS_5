8abd22b13f57f47a73bd0be1d72c0f21c2aa43a2
Susma Rai <105422708+missRaide@users.noreply.github.com>
2022-12-04 17:00:44 +0100

    Update setup.py
    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='shared',
-      version='0.0.1',
+      version='0.0.2',
       description='This package has shared components.',
       author='Susma Rai',
       author_email='missrai.de@email.com',
